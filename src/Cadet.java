a216ffb0cd7c41b00590188c838fee1978d0cc8e
Andy-Guo-13 <andy.guo21@ycdsbk12.ca>
2021-06-02 00:22:05 -0400

    Cadet class

---
+++ src/Cadet.java
@@ -0,0 +1,53 @@
+public class Cadet extends Person{
+
+    // Delcaring variables
+    private String[] summerTraining = new String[0];
+    private String role;
+
+    // Constructor
+    public Cadet(String name, String password, String rank, String role) {
+        super(name, password, rank);
+        this.setRole(role);
+    }
+
+    // Getters and setters
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String[] getSummerTraining() {
+        return summerTraining;
+    }
+
+    public void setSummerTraining(String[] summerTraining) {
+        this.summerTraining = summerTraining;
+    }
+    
+    // Adding a course for summer training
+    public void addSummerCourse(String course) {
+        // Taking the length of the summer training array
+        int length = this.summerTraining.length;
+
+        // Declaring a string array 1 longer than the summer training array
+        String[] tempArray = new String[length + 1];
+
+        // Counter variable
+        int counter = 0;
+
+        // Loading the summer training array into the string array
+        for (String i : this.summerTraining) {
+            tempArray[counter] = i;
+            counter++;
+        }
+
+        // Adding the new course into the last index of the array
+        tempArray[counter] = course;
+
+        // Changing the summer training array to the temporary array
+        this.setSummerTraining(tempArray);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        Person person = new Person("Andy Guo", "password", "WO2");
+        Cadet person = new Cadet("Andy Guo", "password", "WO2", "Cadet");
         System.out.println(person);
         System.out.println(person.getEmail());
         System.out.println(person.getPassword());
+
+        //person.addSummerCourse("Survival instructor");
+        System.out.println(Arrays.toString(person.getSummerTraining()));
     }
 }
